b2b61a82d83f0087c89e5846f3bd8a4da335828f
Cortney Lane <cdlane124@gmail.com>
2017-01-04 21:09:36 -0800

    more props and state fun

--- App.jsx
+++ App.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      header: "Header from props",
-      content: "Content from props"
-    }
-  }
-
   render() {
     return (
       <div>
-        <Header headerProp = {this.state.header} />
-        <Content contentProp = {this.state.content} />
+        <h1>{this.props.header}</h1>
+        <p>Array: {this.props.propArray}</p>
+        <p>Bool: {this.props.propBool ? "True dat" : "FALSE"}</p>
+        <p>Integer: {this.props.propInt}</p>
+        <p>Object: {this.props.propObject.objectName1}</p>
+        <p>Object: {this.props.propObject.objectName2}</p>
       </div>
     );
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.headerProp}</h1>
-      </div>
-    );
-  }
+App.propTypes = {
+  header: React.PropTypes.string,
+  propArray: React.PropTypes.array.isRequired,
+  propBool: React.PropTypes.bool.isRequired,
+  propInt: React.PropTypes.number,
+  propObject: React.PropTypes.object
 }
 
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.contentProp}</p>
-      </div>
-    );
+App.defaultProps = {
+  header: "Some types of datuhhh",
+  propArray: [1,3,5,7,9],
+  propBool: true,
+  propInt: 3,
+  propObject: {
+    objectName1: "Name number one",
+    objectName2: "Name number two"
   }
 }
 
